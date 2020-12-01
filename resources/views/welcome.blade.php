a1ecefb4b9f7f7c9308a2ceef867c6fbd3f465f1
Jesus Raul <al221910356@gmail.com>
2020-12-01 00:58:57 -0600

    Vue

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,24 @@
 @extends('layouts.app')
 
-@section('title', 'Home')
 @section('content')
 <div class="container">
-    <div class="row">
-        <h1>Bienvenido a MiLaravel</h1>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                   
+                    <post-component></post-component>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
